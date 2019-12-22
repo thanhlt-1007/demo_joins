d4d1465542ecaec1f3cd7ae677458e8042578dab
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-12-22 23:31:12 +0700

    add_scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_many :orders, dependent: :destroy
+
+  scope :order_order_location_bid, (lambda do
+    order_location_ids = Order.includes(:order_locations).map(&:order_location_ids)
+    min_order_location_ids = order_location_ids.map(&:min).join(", ")
+    max_order_location_ids = order_location_ids.map(&:max).join(", ")
+
+    joins(:orders)
+      .joins("INNER JOIN order_locations order_locations_1
+        ON order_locations_1.order_id == orders.id
+          AND order_locations_1.id IN (#{min_order_location_ids})")
+      .joins("INNER JOIN order_locations order_locations_2
+        ON order_locations_2.order_id == orders.id
+        AND order_locations_2.id IN (#{max_order_location_ids})")
+      .joins("INNER JOIN bids
+        ON bids.order_id == orders.id")
+  end)
 end
